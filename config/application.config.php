d5372d9b84f0ce80b903ba546e004d7058ffe24a
timepuls <markus.wilhelm@timepuls.ch>
2016-01-11 01:27:25 +0100

    first change

--- config/application.config.php
+++ config/application.config.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-    // This should be an array of module namespaces used in the application.
+    // Thisshould be an array of module namespaces used in the application.
     'modules' => array(
         'Application',
     ),
