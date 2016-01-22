43ce36534615835cca38a9e73787ac29f79484e4
timepuls <markus.wilhelm@timepuls.ch>
2016-01-22 17:59:58 +0100

    add new module Album 2

---
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+/**
+ * {0}
+ * 
+ * @author
+ * @version 
+ */
+class AlbumController extends AbstractActionController
+ {
+     public function indexAction()
+     {
+     }
+
+     public function addAction()
+     {
+     }
+
+     public function editAction()
+     {
+     }
+
+     public function deleteAction()
+     {
+     }
+ }
